d118802359ffca2e475e75fbbb37b40fe92553d5
Sheetal Gandhi <sheetal.gandhi@wwt.com>
2021-07-29 16:04:00 +0530

    Perf files

---
+++ QuickRestaurant.QA.Tests/performance/HealthCheck/SpikeTest.js
@@ -0,0 +1,35 @@
+import http from "k6/http";
+import { Rate } from 'k6/metrics';
+import { check } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: "7.5s", target: 1 },
+        { duration: "7.5s", target: 1 },
+        { duration: "7.5s", target: 10 },
+        { duration: "30s", target: 10 },
+        { duration: "7.5s", target: 0 }
+      ],
+      summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(95)', 'p(99)'],
+      thresholds: { http_req_duration: ["p(99)<5000"],
+          SuccessRate:["rate<10"]
+      }
+  }
+
+var printed = false;
+var successRate = new Rate('SuccessRate');
+export default function main() {
+    let response;
+    if(!printed){
+        console.log("URL: "+ `http://${__ENV.QSR__API_BASEURL}/healthcheck`);
+        printed = true;
+    } 
+    response = http.get(`http://${__ENV.QSR__API_BASEURL}/healthcheck`);
+
+    check(response, {
+        'Spike: is status 200': (r) => r.status === 200,
+    });
+
+    successRate.add(response.status === 200);
+
+}
---
+++ QuickRestaurant.QA.Tests/performance/HealthCheck/StressTest.js
@@ -0,0 +1,35 @@
+import http from "k6/http";
+import { Rate } from 'k6/metrics';
+import { check } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: "12s", target: 5 },
+        { duration: "12s", target: 5 },
+        { duration: "12s", target: 10 },
+        { duration: "12s", target: 10 },
+        { duration: "12s", target: 0 }
+      ],
+      summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(95)', 'p(99)'],
+      thresholds: { http_req_duration: ["p(99)<5000"],
+          SuccessRate:["rate<10"]
+      }
+  }
+
+var printed = false;
+var successRate = new Rate('SuccessRate');
+export default function main() {
+    let response;
+    if(!printed){
+        console.log("URL: "+ `http://${__ENV.QSR__API_BASEURL}/healthcheck`);
+        printed = true;
+    } 
+    response = http.get(`http://${__ENV.QSR__API_BASEURL}/healthcheck`);
+
+    check(response, {
+        'Stress: is status 200': (r) => r.status === 200,
+    });
+
+    successRate.add(response.status === 200);
+
+}
